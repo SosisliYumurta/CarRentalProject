e6fde4115e98214617b904577ff2bb00cdf5c3b4
Oguz Berk AYDIN <ouzberkaydin@gmail.com>
2023-08-06 02:24:46 +0300

    Added new Result class

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Concrete;
+using Core.Utilities.Results.Concrete;
 using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
@@ -15,29 +16,34 @@ namespace ConsoleUI
 
 
 
-            CarManager  carManager = new CarManager(efCarDal);
-            Car car1 = new Car();
-            //car1.Id = 5;
-            car1.CarName = "Foo";
-            car1.DailyPrice = 10;
-            car1.BrandId = 4;
-            car1.ColorId = 4;
-            car1.ModelYear = "2001";
-            car1.Description = "Test";
-            //efCarDal.Add(car1);
-            car1 = carManager.GetCar(7);
-            //car1.ColorId = 4;
+            //CarManager  carManager = new CarManager(efCarDal);
+            //Car car1 = new Car();
+            ////car1.Id = 5;
+            //car1.CarName = "Foo";
+            //car1.DailyPrice = 10;
             //car1.BrandId = 4;
-            //efCarDal.Update(car1);
-            efCarDal.Delete(car1);
+            //car1.ColorId = 4;
+            //car1.ModelYear = "2001";
+            //car1.Description = "Test";
+            ////efCarDal.Add(car1);
+            //car1 = carManager.GetCar(7);
+            ////car1.ColorId = 4;
+            ////car1.BrandId = 4;
+            ////efCarDal.Update(car1);
+            ////efCarDal.Delete(car1);
        
-            foreach (var car in carManager.GetCarDetails())
-            {
-                Console.WriteLine(car.CarName+""+car.DailyPrice+" "+ car.BrandName+" "+car.ColorName);
-                //Console.WriteLine(car.CarName+" "+car.ColorId);
-            }
+            //foreach (var car in carManager.GetCarDetails())
+            //{
+            //    Console.WriteLine(car.CarName+""+car.DailyPrice+" "+ car.BrandName+" "+car.ColorName);
+            //    //Console.WriteLine(car.CarName+" "+car.ColorId);
+            //}
 
-            //Console.WriteLine(carManager.GetCar(5).CarName);
+            ////Console.WriteLine(carManager.GetCar(5).CarName);
+            ///
+
+
+            SuccessResult result = new SuccessResult("asd");
+            Console.WriteLine(result.Success+ result.Message);
 
 
 
